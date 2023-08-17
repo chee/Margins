1d2302151a802ae036eba60ae66f895e457222ae
chee <yay@chee.party>
2023-08-18 00:23:22 +0100

    Initial Commit

---
+++ Margins/MarginsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarginsApp.swift
+//  Margins
+//
+//  Created by chee rabbits on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarginsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
